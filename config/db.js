397f77087052e096f9d513b6465eb1d6825b61f5
Arjun Bhut <arjunbhut@Arjuns-MacBook-Air.local>
2020-11-05 00:29:32 +0530

    make it better

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const connection = async() => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+         
+    }
+    catch{
+        console.error(err)
+        process.exit(1)
+    }
+}
+
+module.exports = connection;
